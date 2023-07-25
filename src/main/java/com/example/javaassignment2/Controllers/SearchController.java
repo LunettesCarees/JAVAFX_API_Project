1d0bac38330168818692cb035f61c6741f29ecb8
Guanzhou Chen <guanzhouchen11@gmail.com>
2023-07-24 23:58:46 -0400

    Add search country view and controller

---
+++ src/main/java/com/example/javaassignment2/Controllers/SearchController.java
@@ -0,0 +1,17 @@
+package com.example.javaassignment2.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class SearchController {
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    void detailsButton_onclick(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.example.javaassignment2 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.javaassignment2 to javafx.fxml;
     exports com.example.javaassignment2;
+    exports com.example.javaassignment2.Controllers;
+    opens com.example.javaassignment2.Controllers to javafx.fxml;
 }
